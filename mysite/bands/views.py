e5027edbf1ad12d44deb71b24255417ef5916971
Attany <nathalyaraujoo@gmail.com>
2016-06-15 21:26:47 -0300

    Add function band_listing

--- mysite/bands/views.py
+++ mysite/bands/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+ffrom django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Band, Member
 
-# Create your views here.
+def home(request):
+    return HttpResponse('Welcome to the site!')
+
+def band_listing(request):
+    """ A view of all bands. """
+    bands = Band.objects.all()
+    var_get_search = request.GET.get('search_box')
+    if var_get_search is not None:
+        bands = bands.filter(name__icontains=var_get_search)
+    return render(request, 'bands/band_listing.html', {'bands': bands})
