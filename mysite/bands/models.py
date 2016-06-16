2b10fc73f4c0229eeb09ee95c01ff040d113c96b
Attany <nathalyaraujoo@gmail.com>
2016-06-15 21:13:17 -0300

    Create models Band

--- mysite/bands/models.py
+++ mysite/bands/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Band(models.Model):
+
+    """A model of a rock band."""
+    name = models.CharField(max_length=200)
+    can_rock = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'band'
+        verbose_name_plural = 'bands'
+
+    def __str__(self):
+        return self.name
+
+    
